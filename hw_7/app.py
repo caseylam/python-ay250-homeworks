0a540f4334cc163d647a7e6c9bb32ed86d626b5b
Default user <casey_lam@berkeley.edu>
2022-04-11 21:49:25 +0000

    basic flask app has all working structures in place

--- hw_7/app.py
+++ hw_7/app.py
@@ -5,6 +5,7 @@ import numpy as np
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 from pybtex.database import parse_file 
+import sqlite3
 
 """
 Useful examples: https://stackoverflow.com/questions/62004957/crating-an-if-statement-for-different-html-option-in-flask-python
@@ -18,6 +19,103 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def parse_bib_to_df(bibfile, collection_name):
+    """
+    Parse a bibtex file into a pandas dataframe.
+    Keeps only the tag, author, journal, volume, pages, year,
+    (For easy insertion into an SQL database later.)
+    
+    bibfile : a .bib file
+    collection_name : string
+    """
+    bib_data = parse_file(bibfile, "bibtex")
+
+    tag_list = []
+    author_list = []
+    journal_list = []
+    volume_list = []
+    pages_list = []
+    year_list = []
+    title_list = []
+    collection_list = []
+
+    for tag in list(bib_data.entries.keys()):
+        if 'Author' in bib_data.entries[tag].persons:
+            for author_name in list(bib_data.entries[tag].persons['Author']):
+                author = author_name.last_names[0][1:-1]
+                if 'journal' in bib_data.entries[tag].fields._dict:
+                    journal = bib_data.entries[tag].fields._dict['journal'][1:]
+                else:
+                    journal = np.nan
+
+                if 'volume' in bib_data.entries[tag].fields._dict: 
+                    volume = int(bib_data.entries[tag].fields._dict['volume'])
+                else:
+                    journal = np.nan
+
+                if 'pages' in bib_data.entries[tag].fields._dict: 
+                    pages = bib_data.entries[tag].fields._dict['pages']
+                else:
+                    journal = np.nan
+
+                if 'year' in bib_data.entries[tag].fields._dict: 
+                    year = int(bib_data.entries[tag].fields._dict['year'])
+                else:
+                    journal = np.nan
+
+                if 'title' in bib_data.entries[tag].fields._dict: 
+                    title = bib_data.entries[tag].fields._dict['title'][1:-1]
+                else:
+                    journal = np.nan
+
+                tag_list.append(tag)
+                author_list.append(author)
+                journal_list.append(journal)
+                volume_list.append(volume)
+                pages_list.append(pages)
+                year_list.append(year)
+                title_list.append(title)
+                collection_list.append(collection_name)
+
+    df = pd.DataFrame({'tag': tag_list, 'author': author_list,
+                        'journal' : journal_list, 'volume' : volume_list,
+                        'pages' : pages_list, 'year' : year_list,
+                        'title' : title_list, 'collection' : collection_list})
+    
+    return df
+
+def df_to_sql(df):
+    # FIXME: SAVE THIS TO A PARTICULAR PLACE??
+    connection = sqlite3.connect("bibliography.db")
+
+    cursor = connection.cursor()
+
+    sql_cmd = """CREATE TABLE bibliography 
+                (iid INTEGER  NOT NULL  PRIMARY KEY  AUTOINCREMENT DEFAULT 0, 
+                tag TEXT, 
+                author TEXT, 
+                journal TEXT, 
+                volume FLOAT, 
+                pages TEXT,
+                year FLOAT,
+                title TEXT,
+                collection TEXT)"""
+
+    cursor.execute(sql_cmd)
+    connection.commit()
+    
+    df = df.where(pd.notnull(df), None)
+    for ii, row in df.iterrows():
+        iparams = (row['tag'], row['author'], row['journal'], row['volume'], 
+                   row['pages'], row['year'], row['title'], row['collection'])
+        sql_cmd = """INSERT INTO bibliography
+                    (tag, author, journal, volume, pages, year, title, collection)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+
+        cursor.execute(sql_cmd, iparams)
+    connection.commit()
+    return
 
 @app.route('/', methods=['GET', 'POST'])
 def start_page():
@@ -44,15 +142,39 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            collection_name = request.form['collname']
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('download_file', name=filename))
+            bibfile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(bibfile)
+            
+            # Save bib into dataframe
+            bibdf = parse_bib_to_df(bibfile, collection_name)
+            df_to_sql(bibdf)
+            
             return redirect(url_for('start_page'))
     return render_template('upload_file.html')
 
+@app.route('/results')
+def display_query_results(db_info):
+    """
+    db_info : the result of the SQL query.
+    """
+    return render_template('display.html', len = len(db_info), db_info = db_info)
+    
 @app.route('/query', methods=['GET', 'POST'])
 def query_db():
-    return
+    if request.method == 'POST':
+        connection = sqlite3.connect("bibliography.db")
+        cursor = connection.cursor()
+        sql_cmd = request.form['query']
+        cursor.execute(sql_cmd)
+
+        db_info = cursor.fetchall()
+        print(db_info)
+        
+        return render_template('display.html', len = len(db_info), db_info = db_info)
+
+    return render_template('query.html')
 
 @app.route('/uploads/<name>')
 def download_file(name):
